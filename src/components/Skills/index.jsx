f59abe0ea0ece0df5305d393b2753381beb569de
Marcos Antonio <antoniosantana.marcos28@gmail.com>
2023-01-01 18:15:57 -0300

    skills

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,10 +1,45 @@
-import { DiReact } from "react-icons/di";
+import {DiHtml5, DiCss3, DiJsBadge, DiSass, DiReact, DiGit } from "react-icons/di";
 import "./style.scss"
 
 export default function Skills(){
     return(
         <section id="skills" className="skills">
-            <DiReact className="cor"/>
+
+            <h2>Skills</h2>
+
+            <div className="skills-container">
+
+                <figure className="skill-format">
+                    <DiHtml5 className="skills-icon"/>
+                    <span>HTML</span>
+                </figure>
+
+                <figure className="skill-format">
+                    <DiCss3 className="skills-icon"/>
+                    <span>CSS</span>
+                </figure>
+
+                <figure className="skill-format">
+                    <DiJsBadge className="skills-icon"/>
+                    <span>JAVASCRIPT</span>
+                </figure>
+
+                <figure className="skill-format">
+                    <DiSass className="skills-icon"/>
+                    <span>SASS</span>
+                </figure>
+
+
+                <figure className="skill-format">
+                    <DiReact className="skills-icon"/>
+                    <span>REACT</span>
+                </figure>
+
+                <figure className="skill-format">
+                    <DiGit className="skills-icon"/>
+                    <span>GIT</span>
+                </figure>
+            </div>
         </section>
     )
 }
